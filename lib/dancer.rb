f1344189f7f34c40acc176cb1c23c9b38a622610
Deborah Santillan <s.debb@yahoo.com>
2020-07-26 16:17:15 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -4,6 +4,7 @@ require_relative './dance_module.rb'
 class Dancer
   include Dance       #=> "include" - These are instance methods
   extend MetaDancing  #=> "extend"  - These are class methods
+  
   attr_accessor :name
   def initialize(name)
     @name = name
